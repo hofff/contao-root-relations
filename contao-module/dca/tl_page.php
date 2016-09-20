0f63650006e17c8e94a7664a2798eecfb785428c
Oliver Hoff <oliver@hofff.com>
2016-09-20 06:50:59 +0200

    fix refactoring error from 92bca03a96b658489b26bcc2ba9f4ddb9cd4c622

--- contao-module/dca/tl_page.php
+++ contao-module/dca/tl_page.php
@@ -4,7 +4,7 @@ $config = &$GLOBALS['TL_DCA']['tl_page']['config'];
 foreach([ 'oncreate', 'onsubmit', 'onrestore', 'oncopy', 'oncut' ] as $callback) {
 	$key = $callback . '_callback';
 	$config[$key] = (array) $config[$key];
-	array_unshift($config[$key], [ PageDCA::class, $callback . 'Page' ]);
+	array_unshift($config[$key], [ \Hofff\Contao\RootRelations\PageDCA::class, $callback . 'Page' ]);
 }
 $config['sql']['keys']['hofff_root_page_id'] = 'index';
 unset($config);
