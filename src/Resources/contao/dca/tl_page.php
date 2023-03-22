67380b1cee3f62247ccf69378831f90ca96d8a41
David Molineus <david.molineus@netzmacht.de>
2023-03-22 15:39:36 +0100

    Handle non existing callbacks

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -7,7 +7,7 @@ use Hofff\Contao\RootRelations\PageDCA;
 $config = &$GLOBALS['TL_DCA']['tl_page']['config'];
 foreach (['oncreate', 'onsubmit', 'onrestore', 'oncopy', 'oncut'] as $callback) {
     $key          = $callback . '_callback';
-    $config[$key] = (array) $config[$key];
+    $config[$key] = (array) ($config[$key] ?? []);
     array_unshift($config[$key], [PageDCA::class, $callback . 'Page']);
 }
 
